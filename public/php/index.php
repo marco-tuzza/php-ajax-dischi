0c6920248aa2111f5c22f3d296d2514a01db92b0
Marco Tuzza <marcotuzza@hotmail.it>
2020-06-17 17:37:20 +0200

    Aggiunta header

--- public/php/index.php
+++ public/php/index.php
@@ -7,11 +7,29 @@ include "dischi.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YouTube Music</title>
+    <title>YouTube Music PHP</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <img src="https://www.gabrielerapino.com/it/files/2018/11/youtube-logo.png" alt="">
+            <span>Music</span>
+        </div>
+        <div class="nav">
+            <ul>
+                <li id="home">Home</li>
+                <li id="dropdown">Generi</li>
+                <li id="search">Cerca</li>
+            </ul>
+            <input type="text" name="" value="" placeholder="Cerca">
+            <i class="fas fa-search"></i>
+        </div>
+        <div class="user">
+            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="">
+        </div>
+    </header>
     <main>
         <?php  
         foreach ($dischi as $brano ) {
